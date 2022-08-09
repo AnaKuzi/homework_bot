c3a3a1bfc1197b17352e1c3d29039a944eb32765
Anastasia Kuzina <anastasia.kuzina88@gmail.com>
2022-08-09 20:15:53 +0300

    4 iteration

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,10 @@
 class HTTPRequestError(Exception):
-    'Basic exception'
+    """Basic exception."""
 
 
 class AnotherEndpointException(Exception):
-    'Some Endpoint errors'
+    """Some Endpoint errors."""
+
+
+class SendMessageException(Exception):
+    """Can't send the message."""
--- homework.py
+++ homework.py
@@ -10,7 +10,8 @@ import telegram
 from dotenv import load_dotenv
 from requests import RequestException
 
-from exceptions import AnotherEndpointException, HTTPRequestError
+from exceptions import (AnotherEndpointException, HTTPRequestError,
+                        SendMessageException)
 
 load_dotenv()
 
@@ -46,8 +47,7 @@ def send_message(bot: telegram.Bot, message: str) -> None:
         logger.info('Начинаем отправку сообщения')
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except telegram.TelegramError as error:
-        raise error(
-            'Не удалось отправить сообщение')
+        raise SendMessageException(f'Ошибка при отправке сообщения {error}')
     else:
         logger.info(f'Отправлено сообщение: {message}')
 
@@ -77,8 +77,9 @@ def get_api_answer(current_timestamp: int) -> dict:
             'Ошибка преобразования типа данных')
     except RequestException as error:
         raise error(f'Нет доступа к {ENDPOINT}')
-    except AnotherEndpointException as error:
-        raise error(f'Сбой при запросе {ENDPOINT}')
+    except Exception as error:
+        raise AnotherEndpointException(
+            f'Ошибка при запросе к {ENDPOINT}: {error}')
 
 
 def check_response(response: dict) -> list:
@@ -86,13 +87,11 @@ def check_response(response: dict) -> list:
     logger.info('Начинаем проверку ответа сервера')
     if not isinstance(response, dict):
         raise TypeError('Ответ не является словарем')
-    if not response:
-        raise KeyError('Словарь ответа API пуст')
-    if 'homeworks' not in response:
-        raise KeyError('Ключа homeworks не существует')
-    if 'current_date' not in response:
-        raise KeyError('Ключа current_date не существует')
     homeworks = response.get('homeworks')
+    if 'homeworks' not in response.keys():
+        raise KeyError('Ключа homeworks не существует')
+    if 'current_date' not in response.keys():
+        raise KeyError('Ключа current_date не существует')
     if not isinstance(homeworks, list):
         raise KeyError('В homeworks лежит не список')
     return homeworks
@@ -120,35 +119,43 @@ def main():
         sys.exit('Выполнение программы остановлено')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     current_timestamp = int(time.time())
+    old_message = ''
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homeworks = check_response(response)
             if homeworks:
                 message = parse_status(homeworks[0])
-                send_message(bot, message)
-            else:
-                logger.debug('Новый статус отсутствует')
-            current_timestamp = response.get('current_date')
+                if message != old_message:
+                    send_message(bot, message)
+                    old_message = message
+                    current_timestamp = response.get('current_date')
+                else:
+                    logger.debug('Новый статус отсутствует')
 
+        except SendMessageException as error:
+            logger.error(
+                f'Ошибка при отправке сообщения {error}')
         except HTTPRequestError as error:
-            logger.warning(
+            logger.error(
                 f'Ошибка {error}'
                 f'Эндпоинт {response.url} недоступен. '
                 f'Код ответа API: {response.status_code}')
         except AnotherEndpointException as error:
-            logger.warning(
+            logger.error(
                 f'Ошибка {error}'
                 f'Сбой при запросе {ENDPOINT}')
         except RequestException as error:
-            logger.warning(
+            logger.error(
                 f'Ошибка {error}'
                 f'Ошибка запроса {ENDPOINT}.')
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            send_message(bot, message)
-            logger.debug(message)
+            if message != old_message:
+                send_message(bot, message)
+                old_message = message
+                logger.error(message)
 
         finally:
             time.sleep(RETRY_TIME)
